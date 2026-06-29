c563e19b6794d34c7d77ab073b34d7b5ea76b6a6
Beilong Tang <btang5@ncsu.edu>
2026-06-29 16:38:34 -0400

    update [auto]

--- ImageGeneration/run_lib_pytorch.py
+++ ImageGeneration/run_lib_pytorch.py
@@ -65,7 +65,6 @@ def train(config, workdir):
   score_model = mutils.create_model(config)
   ema = ExponentialMovingAverage(score_model.parameters(), decay=config.model.ema_rate)
   optimizer = losses.get_optimizer(config, score_model.parameters())
-
   state = dict(optimizer=optimizer, model=score_model, ema=ema, step=0)
 
   # Create checkpoints directory
